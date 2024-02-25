636809e1b9eb13ea001ba7bc70e19bb1d8c773b0
Rennan bacili <132062465+RennanBaccili@users.noreply.github.com>
2024-02-25 15:01:40 -0300

    código para criar imagens para o pygame a partir de texto

--- code/Game.py
+++ code/Game.py
@@ -2,16 +2,18 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+
 from code.Menu import Menu
+from code.Const import WIN_H, WIN_W
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(576, 324))
+        self.window = pygame.display.set_mode(size=(WIN_W, WIN_H))
 
     def run(self):
+
         while True:
             menu = Menu(self.window)
             menu.run()
-            pass
--- code/Menu.py
+++ code/Menu.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame.image
-from pygame import Surface
+from pygame import Surface, Rect
+from pygame.font import Font
+from code.Const import WIN_W
 
 
 class Menu:
@@ -10,8 +12,25 @@ class Menu:
         self.surf = pygame.image.load('./asset/menu_bg.png')
         self.rect = self.surf.get_rect(left=0, top=0)
 
-    def run(self, ):
+    def run(self):
+        # inicio o som do menu
+        pygame.mixer_music.load('./asset/menu_song.mp3')
+        pygame.mixer_music.play(-1)
+        pygame.mixer_music.set_volume(0.15)
+
+        # imagem do menu
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "Mountain", (255, 128, 0), ((WIN_W / 2), 70))
             pygame.display.flip()
-        pass
+
+    def menu_text(self,
+                  text_size: int,
+                  text: str,
+                  text_color: tuple,
+                  text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont("Lacida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color)
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
+        # para atualizar a imagem, que no caso é texto criado
