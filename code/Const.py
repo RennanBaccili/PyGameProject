3436de92e7c8843e7454527029101b18639ef5a5
Rennan bacili <132062465+RennanBaccili@users.noreply.github.com>
2024-02-26 08:12:21 -0300

    Movimento paralax, fase1 Feito

--- code/Const.py
+++ code/Const.py
@@ -1,10 +1,9 @@
-
 # Colors
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
 COLOR_YELLOW = (255, 255, 128)
 
-#Menu
+# Menu
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
                'NEW GAME 2P - COMPETITIVE',
@@ -14,5 +13,13 @@ MENU_OPTION = ('NEW GAME 1P',
 WIN_W = 576
 WIN_H = 324
 
-#i
+# i
 IMAGE_LEVEL = {'level1': 5}
+
+# speed
+ENTITY_SPEED = {'m1/1.png': 0,
+                'm1/2.png': 1,
+                'm1/3.png': 2,
+                'm1/4.png': 3,
+                'm1/5.png': 4,
+                }
--- code/Level.py
+++ code/Level.py
@@ -15,7 +15,9 @@ class Level:
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1'))
 
-    def run(self):
+
+
+    def run(self, ):
         while True:
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
